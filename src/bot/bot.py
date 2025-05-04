25834a5a54929abd399ff1ad7e93f24295411d94
0xJakuzya <kuzmin_dima2005@mail.ru>
2025-05-04 17:12:41 +0400

    a

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -4,6 +4,7 @@ from aiogram.filters import Command
 from config import config
 from parser.api import API
 from parser.models import Vacancy
+from db import DataBase
 
 import logging
 import asyncio
@@ -14,6 +15,7 @@ class VacancyBot:
 
         self.bot = Bot(token=config.BOT_TOKEN)
         self.api = API()
+        self.db = DataBase()
         self.dp = Dispatcher()
         self.register_handlers()
 
@@ -21,7 +23,6 @@ class VacancyBot:
 
         self.dp.message.register(self.send_welcome, Command("start"))
         self.dp.message.register(self.send_vacancies, Command("vacancies"))
-        self.dp.message.register(self.search_vacancies, Command("search"))
 
     async def send_welcome(self, message: types.Message):
 
@@ -29,7 +30,6 @@ class VacancyBot:
             "Привет! Я бот для поиска вакансий.\n"
             "Используйте команды:\n"
             "/vacancies - вакансии по умолчанию\n"
-            "/search <запрос> <регион> - кастомный поиск"
         )
 
     async def send_vacancies(self, message: types.Message):
@@ -49,29 +49,6 @@ class VacancyBot:
             logging.error(f"Ошибка в send_vacancies: {e}")
             await message.reply(f"Произошла ошибка при обработке запроса: {e}")
 
-    async def search_vacancies(self, message: types.Message): #пока не работает!
-        
-        try:
-            _, query, area = message.text.split(maxsplit=2)
-            area = int(area)
-
-        except ValueError:
-            await message.reply("Используйте: /search <запрос> <id региона>")
-            return
-
-        try:
-            vacancies = self.api.get_vacancies(query, area)
-
-            if not vacancies:
-                await message.reply("По вашему запросу ничего не найдено")
-                return
-
-            for vacancy in vacancies:
-                await message.reply(vacancy.formatted()) 
-
-        except Exception as e:
-            logging.error(f"Ошибка в search_vacancies: {e}")
-            await message.reply(f"Произошла ошибка при обработке запроса: {e}")
     
     async def run(self):
         await self.dp.start_polling(self.bot)
--- src/config.py
+++ src/config.py
@@ -9,6 +9,13 @@ class Config:
         
         self.HH_API_URL = "https://api.hh.ru/vacancies"
         self.BOT_TOKEN = os.getenv("BOT_TOKEN")   
+
+        self.DB_USER = os.getenv("DB_USER")
+        self.DB_PASSWORD = os.getenv("DB_PASSWORD")
+        self.DB_NAME = os.getenv("DB_NAME")
+        self.DB_HOST = os.getenv("DB_HOST")
+        self.DB_PORT = os.getenv("DB_PORT")
+
         self.DEFAULT_SEARCH_QUERY = "Data Scientist"
         self.DEFAULT_AREA_ID = 78  
         self.MAX_VACANCIES = 100
